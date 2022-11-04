104d0b3dbb2d2a325a3b00987404f66ac7f978ec
Ulisses Mesquita <ulisses.mesquita@xpon.com.br>
2022-11-04 08:40:36 -0300

    Alterada porta de Teste 3001 para 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const padraoRoute = require('./routes/padrao.js');
 
 const app = express()
-const port = 3001
+const port = 80
 
 /* Padrão Rota */
 app.use(padraoRoute);
